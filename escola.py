9996eb586695cda82309183397f7a7608204a957
Ana Laura <ana.machioni@aluno.senai.br>
2026-03-06 17:28:47 -0300

    fazendo aqui o test, happy patch e erros, sextandoooo

--- escola.py
+++ escola.py
@@ -1,6 +1,15 @@
 def verificador_media(media:int|float) ->str:
     """Esta função retorna se o aluno passou ou nao  de ano"""
 
+    # testando p ver se a media é um numero 
+    if  not isinstance(media,(int, float)):
+        raise TypeError("Tipo invalido, a entrada deve ser numerica")
+
+    if media < 0:
+        raise ValueError("O valor deve ser maior ou igual a 0 e menor ou igual a 10")
+    
+    if media > 10:
+        raise ValueError("O valor deve ser maior ou igual a 0 e menor ou igual a 10")
     if media >= 7:
         return "Aprovado"
     elif media < 5:
@@ -10,6 +19,3 @@ def verificador_media(media:int|float) ->str:
     
 if __name__ =="__main__":
     print(verificador_media(5))
-
-
-
--- test/test_verificador_media_erros.py
+++ test/test_verificador_media_erros.py
@@ -5,3 +5,12 @@ from escola import verificador_media
 def test_string_como_entrada():
     with pytest.raises(TypeError, match="Tipo invalido, a entrada deve ser numerica"):
         verificador_media("OITO")
+
+
+def test_numero_negativo():
+    with pytest.raises(ValueError, match="O valor deve ser maior ou igual a 0 e menor ou igual a 10"):
+        verificador_media(-5)
+
+def test_numero_maior_10():
+    with pytest.raises(ValueError, match="O valor deve ser maior ou igual a 0 e menor ou igual a 10"):
+        verificador_media(25)
